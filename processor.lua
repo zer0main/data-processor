3a6ab1427fcb64b27272c16e69025569d3f7f74f
Pavel Dolgov <pdolgov99@gmail.com>
2015-05-12 22:09:14 +0300

    Function calculateDistance()
    This function returns distance (calculated using formula).

---
+++ processor.lua
@@ -0,0 +1,14 @@
+-- data-processor, script for the processing of
+-- experimental data
+-- Copyright (C) 2015 Pavel Dolgov
+--
+-- See the LICENSE file for terms of use.
+
+local function calculateDistance(height, angle,
+                                 cof)
+    local exponent = math.exp(-2 * angle * cof)
+    local cotangent = 1 / math.tan(angle)
+    local distance = height * exponent *
+        (1 / cof - cotangent)
+    return distance
+end
